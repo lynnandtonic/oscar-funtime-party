6f1293327940ebbe539aaf13f6589b55955710c9
Jake Heun <jake.heun@meltmedia.com>
2018-01-24 18:48:51 -0700

    Let the category list request the information it needs
    The store was firing the original requests, which is silly cakes, yo

--- src/modules/categories/containers/category-list-container.js
+++ src/modules/categories/containers/category-list-container.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import CategoryList from '../components/category-list';
-import CategoryModel from '../models'
+import CategoryModel from '../models';
+import store from '../../../store';
+import { getCategories } from '../../../modules/categories/api';
+import { getNominees } from '../../../modules/nominees/api';
 
-class CategoryListContainer extends React.Component {
+class CategoryListContainer extends Component {
+
+  componentDidMount() {
+    store.dispatch(getCategories());
+    store.dispatch(getNominees());
+  }
 
   render() {
     return (
--- src/store.js
+++ src/store.js
@@ -2,9 +2,6 @@ import { applyMiddleware, createStore, compose } from 'redux';
 import reducers from './rootReducer';
 import createLogger from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
-import { getCategories } from './modules/categories/api';
-import { getNominees } from './modules/nominees/api';
-import firebase, { auth, provider } from './firebase.js';
 
 const loggerMiddleware = createLogger();
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
@@ -19,8 +16,5 @@ const store = createStore(
   reducers,
   enhancer
 );
-
-store.dispatch(getCategories());
-store.dispatch(getNominees());
 
 export default store;
